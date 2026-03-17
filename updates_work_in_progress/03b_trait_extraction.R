8ae37e52bb30e6fa6f0513a130bbda29f3dde298
Anne G. Hertel <anne.g.hertel@gmail.com>
2026-03-17 12:48:53 +0100

    recoding trait extraction script adding monthly summaries and gridded spatial annotation

--- updates_work_in_progress/03b_trait_extraction.R
+++ updates_work_in_progress/03b_trait_extraction.R
@@ -1,15 +1,15 @@
 # ---
 # title: "MoveTraits Database"
 # author: "Anne G. Hertel"
-# date: "28/1/2025"
+# date: "17/3/2026"
 # ---
 
 library(lubridate);library(metafor);library(tidyverse);library(amt);
 library(adehabitatHR); library(move2); library(epitools); library(suncalc); library(purrr); library(bit64)
-library(dggridR);library(mapview);library(ISOweek);library(lubridate)
-
+library(dggridR);library(mapview);library(ISOweek)
 
 ## ----Import functions for traits-------------------------------------------------------------
+
 source(here::here('updates_work_in_progress/trait_scripts', 'd1h.R'))
 source(here::here('updates_work_in_progress/trait_scripts', 'd24h.R'))
 source(here::here('updates_work_in_progress/trait_scripts', 'dmax24h.R'))
@@ -19,10 +19,10 @@ source(here::here('updates_work_in_progress/trait_scripts', 'mcp24h.R'))
 source(here::here('updates_work_in_progress/trait_scripts', 'mcp7d.R'))
 source(here::here('updates_work_in_progress/trait_scripts', 'mcp1m.R'))
 source(here::here('updates_work_in_progress/trait_scripts', 'mcp12m.R'))
-
-# source(here::here('updates_work_in_progress/trait_scripts', 'mcp12m.R'))
-# source(here::here('updates_work_in_progress/trait_scripts', 'mcp12m.R'))
-# source(here::here('updates_work_in_progress/trait_scripts', 'mcp12m.R'))
+source(here::here('updates_work_in_progress/trait_scripts', 'iou24h.R'))
+source(here::here('updates_work_in_progress/trait_scripts', 'iou1m.R'))
+source(here::here('updates_work_in_progress/trait_scripts', 'iou12m.R'))
+source(here::here('updates_work_in_progress/trait_scripts', 'di.R'))
 
 ## ----Import movement data per individual-------------------------------------------------------------
 pathTOfolder <- "./DATA/MoveTraitsData/"
@@ -79,44 +79,43 @@ animlocs.weekly <- animlocs.1hourly |>
   track_resample(rate = hours(24*7),
                  tolerance = minutes(60*24))
 
-## ----Displacement traits-------------------------------------------------------------
+## ----Trait extraction-------------------------------------------------------------
 
-## ----1h displacement-------------------------------------------------------------
+#1h displacement----
 d1h <- calc_d1h(animlocs.1hourly, dggs.10, dggs.1)
 sum.ind.d1h <- f_sum.ind.d1h(d1h)
 sum.monthly.ind.d1h <- f_sum.monthly.ind.d1h(d1h)
 
-## ----Maximum 24hr displacement-------------------------------------------------------------
-dmax24h <- calc_dmax24h(animlocs.1hourly, dggs.10, dggs.1)
-sum.ind.dmax24h <- f_sum.ind.dmax24h(dmax24h)
-sum.monthly.ind.dmax24h <- f_sum.monthly.ind.dmax24h(dmax24h)
-
-## ----24hr displacement distance-------------------------------------------------------------
+#24hr displacement distance----
 d24h <- calc_d24h(animlocs.daily, dggs.10, dggs.1)
 sum.ind.d24h <- f_sum.ind.d24h(d24h)
 sum.monthly.ind.d24h <- f_sum.monthly.ind.d24h(d24h)
 
-## ----Maximum 7day displacement distance-------------------------------------------------------------
+#Maximum 24hr displacement----
+dmax24h <- calc_dmax24h(animlocs.1hourly, dggs.10, dggs.1)
+sum.ind.dmax24h <- f_sum.ind.dmax24h(dmax24h)
+sum.monthly.ind.dmax24h <- f_sum.monthly.ind.dmax24h(dmax24h)
+
+#Maximum 7day displacement distance----
 dmax7d <- calc_dmax7d(animlocs.daily, dggs.10, dggs.1)
 sum.ind.dmax7d <- f_sum.ind.dmax7d(dmax7d)
 sum.monthly.ind.dmax7d <- f_sum.monthly.ind.dmax7d(dmax7d)
 
-## ----Maximum annual displacement distance-------------------------------------------------------------
+#Maximum annual displacement distance----
 dmax12m <- calc_dmax12m(animlocs.weekly, dggs.10, dggs.1)
 sum.ind.dmax12m <- f_sum.ind.dmax12m(dmax12m)
 
-## ----Range size traits-------------------------------------------------------------
-## ----Daily MCP-------------------------------------------------------------
+#Daily MCP----
 mcp24h <- calc_mcp24h(animlocs.1hourly, dggs.10, dggs.1)
 sum.ind.mcp24h <- f_sum.ind.mcp24h(mcp24h)
 sum.monthly.ind.mcp24h <- f_sum.monthly.ind.mcp24h(mcp24h)
 
-## ----Weekly MCP-------------------------------------------------------------
+#Weekly MCP----
 mcp7d <- calc_mcp7d(animlocs.1hourly, dggs.10, dggs.1)
 sum.ind.mcp7d <- f_sum.ind.mcp7d(mcp7d)
 sum.monthly.ind.mcp7d <- f_sum.monthly.ind.mcp7d(mcp7d)
 
-## ----Monthly MCP-------------------------------------------------------------
+#Monthly MCP----
 mcp1m <- calc_mcp1m(animlocs.daily, dggs.10, dggs.1)
 sum.ind.mcp1m <- f_sum.ind.mcp1m(mcp1m)
 sum.monthly.ind.mcp1m <- mcp1m[,1:4] |> 
@@ -124,57 +123,49 @@ sum.monthly.ind.mcp1m <- mcp1m[,1:4] |>
   mutate(year = as.numeric(substr(year_month, 1, 4))) |> 
   dplyr::select(individual_id,month,year,mcp1m)
 
-## ----Annual MCP-------------------------------------------------------------
+#Annual MCP----
 mcp12m <- calc_mcp12m(animlocs.weekly, dggs.10, dggs.1)
 sum.ind.mcp12m <- f_sum.ind.mcp12m(mcp12m)
 
-## ----Intensity of use-------------------------------------------------------------
-
-## ----Daily IOU-------------------------------------------------------------
+#Daily IOU----
 iou24h <- calc_iou24h(mcp24h,d1h, dggs.10, dggs.1)
 sum.ind.iou24h <- f_sum.ind.iou24h(iou24h)
 sum.monthly.ind.iou24h <- f_sum.monthly.ind.iou24h(iou24h)
 
-## ----Monthly IOU-------------------------------------------------------------
+#Monthly IOU----
 iou1m <- calc_iou1m(mcp1m,d24h, dggs.10, dggs.1)
 sum.ind.iou1m <- f_sum.ind.iou1m(iou1m)
-sum.monthly.ind.iou1m <- iou1m[,c(1:3,5)] 
+sum.monthly.ind.iou1m <- iou1m[,c(1,2,3,5)] 
 
-  
+#Annual IOU----
+iou12m <- calc_iou12m(mcp12m,d24h, dggs.10, dggs.1)
+sum.ind.iou12m <- f_sum.ind.iou12m(iou12m)
 
-## ----Annual IOU-------------------------------------------------------------
+#Diurnality Index----
+di <- calc_di(d1h, dggs.10, dggs.1)
+sum.ind.di <- f_sum.ind.di(di)
+sum.monthly.ind.di <- f_sum.monthly.ind.di(di)
 
-
-## ----Diurnality Index-------------------------------------------------------------
-
-
-
-
-## ----Build database with summary values--------------------
+## ----Individual summary database--------------------
 #cbind individual summaries of all traits 
-MoveTrait.v0.1 <- bind_cols(Displ1h, 
-                          bind_cols(Displ24h[,2:length(Displ24h)], 
-                          bind_cols(MaxDispl24h[,2:length(MaxDispl24h)], 
-                          bind_cols(MaxDispl7d[,2:length(MaxDispl7d)], 
-                          bind_cols(MaxDispl12m[,2:length(MaxDispl12m)], 
-                          bind_cols(MCP24h[,2:length(MCP24h)], 
-                          bind_cols(MCP7d[,2:length(MCP7d)], 
-                          bind_cols(MCP1m[,2:length(MCP1m)], 
-                          bind_cols(MCP12m[,2:length(MCP12m)], 
-                          bind_cols(IOU24h[,2:length(IOU24h)],
-                          bind_cols(IOU1m[,2:length(IOU1m)],
-                          bind_cols(IOU12m[,2:length(IOU12m)], DI.12[,2:length(DI.12)])))))))))))) %>% 
+MoveTrait.ind.sum <- bind_cols(sum.ind.d1h, 
+                          bind_cols(sum.ind.d24h[,2:length(sum.ind.d24h)], 
+                          bind_cols(sum.ind.dmax24h[,2:length(sum.ind.dmax24h)], 
+                          bind_cols(sum.ind.dmax7d[,2:length(sum.ind.dmax7d)], 
+                          bind_cols(sum.ind.dmax12m[,2:length(sum.ind.dmax12m)], 
+                          bind_cols(sum.ind.mcp24h[,2:length(sum.ind.mcp24h)], 
+                          bind_cols(sum.ind.mcp7d[,2:length(sum.ind.mcp7d)], 
+                          bind_cols(sum.ind.mcp1m[,2:length(sum.ind.mcp1m)], 
+                          bind_cols(sum.ind.mcp12m[,2:length(sum.ind.mcp12m)], 
+                          bind_cols(sum.ind.iou24h[,2:length(sum.ind.iou24h)],
+                          bind_cols(sum.ind.iou1m[,2:length(sum.ind.iou1m)],
+                          bind_cols(sum.ind.iou12m[,2:length(sum.ind.iou12m)], 
+                                    sum.ind.di[,2:length(sum.ind.di)])))))))))))) %>% 
   filter(if_any(everything(), ~ !is.na(.)))
 
-#mean coordinates for individual summaries
 library(bit64)
 movedata2 <- animlocs.1hourly %>% 
-  mutate(study_id = as.integer64(study_id),
-         individual_id = individual_id) |> 
-  group_by(individual_id) %>% 
-  mutate(mean.longitude = mean(x_,na.rm=T),
-         mean.latitude = mean(y_,na.rm=T)) %>% 
-  dplyr::select(study_id,individual_id,mean.longitude,mean.latitude) %>% 
+  dplyr::select(study_id,individual_id) %>% 
   mutate(individual_id = as.character(individual_id)) %>% 
   distinct()
 
@@ -192,33 +183,73 @@ lat.10km <- dgSEQNUM_to_GEO(dggs.10, grid.id.10km)$lat_deg
 movedata2$grid.id.100km <- paste(grid.id.100km, collapse = ";")
 movedata2$grid.id.10km <- paste(grid.id.10km, collapse = ";")
 
-# join database
-MoveTrait.v0.1 <- 
-  if (is.null(MoveTrait.v0.1) | nrow(MoveTrait.v0.1) == 0) NULL else {
+#join database
+MoveTrait.ind.sum <- 
+  if (is.null(MoveTrait.ind.sum) | nrow(MoveTrait.ind.sum) == 0) NULL else {
     movedata2 %>% 
-    left_join(MoveTrait.v0.1, by = "individual_id") %>% 
+    left_join(MoveTrait.ind.sum, by = "individual_id") %>% 
     droplevels() }
 
-## ----Save database with summaries--------------------
-saveRDS(MoveTrait.v0.1, file=paste0(pthtraitsum,"3809257699_3809647332.rds"))
+#Save database with summaries
+saveRDS(MoveTrait.ind.sum, file=paste0(pthtraitsum,"3809257699_3809647332.rds"))
 
+## ----Individual monthly summary database--------------------
+
+MoveTrait.monthly.ind.sum <- 
+sum.monthly.ind.d1h |>  
+  full_join(sum.monthly.ind.d24h, 
+            by = join_by(individual_id, month, year)) |> 
+  full_join(sum.monthly.ind.dmax24h, 
+            by = join_by(individual_id, month, year)) |> 
+  full_join(sum.monthly.ind.dmax7d, 
+            by = join_by(individual_id, month, year)) |> 
+  full_join(sum.monthly.ind.mcp24h, 
+            by = join_by(individual_id, month, year)) |> 
+  full_join(sum.monthly.ind.mcp7d, 
+            by = join_by(individual_id, month, year)) |> 
+  full_join(sum.monthly.ind.mcp1m, 
+            by = join_by(individual_id, month, year)) |> 
+  full_join(sum.monthly.ind.iou24h, 
+            by = join_by(individual_id, month, year)) |> 
+  full_join(sum.monthly.ind.iou1m, 
+            by = join_by(individual_id, month, year)) |> 
+  full_join(sum.monthly.ind.di, 
+            by = join_by(individual_id, month, year)) |>  
+  filter(if_any(everything(), ~ !is.na(.)))
+  
 # #gridded coordinates for monthly individual summaries
-# id_monthly <- animlocs.1hourly |> 
-#   mutate(month = month(t_),
-#          year = year(t_),
-#          month_year = paste(month,year,sep="_"))
-# 
-# cell_info.100 <- dgGEO_to_SEQNUM(dggs.100, animlocs.1hourly$x_, animlocs.1hourly$y_)
-# id_monthly$grid.id.100km <- cell_info.100$seqnum
-# id_monthly_100km <- id_monthly |> 
-#   distinct(month_year,grid.id.100km,.keep_all = TRUE)
-# 
-# cell_info.10 <- dgGEO_to_SEQNUM(dggs.10, animlocs.1hourly$x_, animlocs.1hourly$y_)
-# id_monthly$grid.id.10km <- cell_info.10$seqnum
-# id_monthly_10km <- id_monthly |> 
-#   distinct(month_year,grid.id.10km,.keep_all = TRUE)
+id_monthly <- animlocs.1hourly |>
+  mutate(month = month(t_),
+         year = year(t_),
+         month_year = paste(month,year,sep="_")) |> 
+  mutate(individual_id = as.character(individual_id))
 
-#pthtraitsummonthly
+cell_info.100 <- dgGEO_to_SEQNUM(dggs.100, animlocs.1hourly$x_, animlocs.1hourly$y_)
+id_monthly$grid.id.100km <- cell_info.100$seqnum
+id_monthly_100km <- id_monthly |>
+  distinct(month_year,grid.id.100km,.keep_all = TRUE)|> 
+  group_by(individual_id,study_id,month, year) |>               
+  summarise(grid.id.100km = paste(grid.id.100km, collapse = ";"),
+            .groups = "drop")
+
+cell_info.10 <- dgGEO_to_SEQNUM(dggs.10, animlocs.1hourly$x_, animlocs.1hourly$y_)
+id_monthly$grid.id.10km <- cell_info.10$seqnum
+id_monthly_10km <- id_monthly |>
+  distinct(month_year,grid.id.10km,.keep_all = TRUE) |> 
+  group_by(individual_id,study_id,month, year) |>               
+  summarise(grid.id.10km = paste(grid.id.10km, collapse = ";"),
+            .groups = "drop")
+
+#join database
+MoveTrait.monthly.ind.sum <- 
+  if (is.null(MoveTrait.monthly.ind.sum) | nrow(MoveTrait.monthly.ind.sum) == 0) NULL else {
+    id_monthly_100km  |>  
+      left_join(id_monthly_10km, by =  join_by(individual_id, study_id, month, year)) %>% 
+      left_join(MoveTrait.monthly.ind.sum, by =  join_by(individual_id, month, year)) %>% 
+      droplevels()  }
+
+#Save database with individual monthly summaries
+saveRDS(MoveTrait.monthly.ind.sum, file=paste0(pthtraitsummonthly,"3809257699_3809647332.rds"))
 
 ## ----Build full database including raw metrics data--------------------
 
